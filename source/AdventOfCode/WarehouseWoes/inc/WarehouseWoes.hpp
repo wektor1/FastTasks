f92da6aa9cf3499c2e22750dd3a133012248b5f4
Mateusz Surowiec <wektor1@gmail.com>
2024-12-16 00:32:29 +0100

    AdventOfCode_Day15_SecondStar

--- source/AdventOfCode/WarehouseWoes/inc/WarehouseWoes.hpp
+++ source/AdventOfCode/WarehouseWoes/inc/WarehouseWoes.hpp
@@ -7,8 +7,9 @@ public:
   unsigned long long run(const std::vector<std::string> &);
 
 private:
-  void move(char, int = 1);
+  bool move(char, std::pair<int, int>, std::vector<std::pair<int, int>> &);
   unsigned long long calculateResultPart1();
+  unsigned long long calculateResultPart2();
 
   std::vector<std::vector<char>> map;
   std::vector<char> operations;
--- source/AdventOfCode/WarehouseWoes/src/WarehouseWoes.cpp
+++ source/AdventOfCode/WarehouseWoes/src/WarehouseWoes.cpp
@@ -1,4 +1,7 @@
 #include "WarehouseWoes.hpp"
+#include <algorithm>
+#include <map>
+#include <utility>
 #include <vector>
 
 namespace {
@@ -16,13 +19,23 @@ unsigned long long WarehouseWoes::run(const std::vector<std::string> &input) {
       continue;
     }
     if (not isOperations) {
-      auto robot = line.find('@');
       std::vector<char> temp{};
-      temp.reserve(line.size());
-      temp.insert(temp.cend(), line.begin(), line.end());
-      if (robot != std::string::npos) {
-        temp[robot] = '.';
-        startPos = {map.size(), robot};
+      temp.reserve(line.size() * 2);
+      for (auto object : line) {
+        if (object == '#' or object == '.') {
+          temp.insert(temp.cend(), 2, object);
+        } else if (object == 'O') {
+          temp.push_back('[');
+          temp.push_back(']');
+        } else if (object == '@') {
+          temp.push_back('@');
+          temp.push_back('.');
+        }
+      }
+      auto robot = std::ranges::find(temp, '@');
+      if (robot != temp.end()) {
+        *robot = '.';
+        startPos = {map.size(), robot - temp.begin()};
       }
       map.push_back(std::move(temp));
     } else {
@@ -30,75 +43,180 @@ unsigned long long WarehouseWoes::run(const std::vector<std::string> &input) {
     }
   }
   for (const auto operation : operations) {
-    move(operation);
+    std::vector<decltype(startPos)> startinList{};
+    move(operation, startPos, startinList);
   }
-  return calculateResultPart1();
+  return calculateResultPart2();
 }
 
-void WarehouseWoes::move(const char direction, const int objectsToMove) {
+bool WarehouseWoes::move(const char direction,
+                         std::pair<int, int> objectToCheck,
+                         std::vector<std::pair<int, int>> &objectsToMove) {
   switch (direction) {
   case up:
-    if (map[startPos.first - objectsToMove][startPos.second] == 'O') {
-      move(up, objectsToMove + 1);
-      return;
-    }
-    if (map[startPos.first - objectsToMove][startPos.second] == '.') {
-      for (int i = 0; i < objectsToMove; i++) {
-        map[startPos.first - objectsToMove + i][startPos.second] =
-            map[startPos.first - objectsToMove + (i + 1)][startPos.second];
+    if (objectToCheck == startPos) {
+      objectsToMove.push_back(objectToCheck);
+      objectToCheck.first--;
+      if (move(direction, objectToCheck, objectsToMove)) {
+        std::map<std::pair<int, int>, char> temp{};
+        for (const auto object : objectsToMove) {
+          temp[object] = map[object.first][object.second];
+        }
+        for (auto tempObjMapped : temp) {
+          map[tempObjMapped.first.first - 1][tempObjMapped.first.second] =
+              tempObjMapped.second;
+          if ((tempObjMapped.second == '[' or tempObjMapped.second == ']') and
+              not temp.contains({tempObjMapped.first.first + 1,
+                                 tempObjMapped.first.second})) {
+            map[tempObjMapped.first.first][tempObjMapped.first.second] = '.';
+          }
+        }
+        startPos.first--;
       }
-      map[startPos.first][startPos.second] = '.';
-      startPos.first--;
-      return;
+      return true;
     }
-    return;
+    if (map[objectToCheck.first][objectToCheck.second] == '[') {
+      objectsToMove.push_back(objectToCheck);
+      std::pair<int, int> secondObjectToCheck = {objectToCheck.first,
+                                                 objectToCheck.second + 1};
+      objectsToMove.push_back(secondObjectToCheck);
+      if (map[secondObjectToCheck.first + 1][secondObjectToCheck.second] ==
+          '.') {
+        objectsToMove.push_back(
+            {secondObjectToCheck.first + 1, secondObjectToCheck.second});
+      }
+      objectToCheck.first--;
+      secondObjectToCheck.first--;
+      return move(up, objectToCheck, objectsToMove) and
+             move(up, secondObjectToCheck, objectsToMove);
+    }
+    if (map[objectToCheck.first][objectToCheck.second] == ']') {
+      objectsToMove.push_back(objectToCheck);
+      std::pair<int, int> secondObjectToCheck = {objectToCheck.first,
+                                                 objectToCheck.second - 1};
+      objectsToMove.push_back(secondObjectToCheck);
+      if (map[secondObjectToCheck.first + 1][secondObjectToCheck.second] ==
+          '.') {
+        objectsToMove.push_back(
+            {secondObjectToCheck.first + 1, secondObjectToCheck.second});
+      }
+      objectToCheck.first--;
+      secondObjectToCheck.first--;
+      return move(up, objectToCheck, objectsToMove) and
+             move(up, secondObjectToCheck, objectsToMove);
+    }
+    if (map[objectToCheck.first][objectToCheck.second] == '.') {
+      return true;
+    }
+    return false;
   case down:
-    if (map[startPos.first + objectsToMove][startPos.second] == 'O') {
-      move(down, objectsToMove + 1);
-      return;
-    }
-    if (map[startPos.first + objectsToMove][startPos.second] == '.') {
-      for (int i = 0; i < objectsToMove; i++) {
-        map[startPos.first + objectsToMove - i][startPos.second] =
-            map[startPos.first + objectsToMove - (i + 1)][startPos.second];
+    if (objectToCheck == startPos) {
+      objectsToMove.push_back(objectToCheck);
+      objectToCheck.first++;
+      if (move(direction, objectToCheck, objectsToMove)) {
+        std::map<std::pair<int, int>, char> temp{};
+        for (const auto object : objectsToMove) {
+          temp[object] = map[object.first][object.second];
+        }
+        for (auto tempObjMapped : temp) {
+          map[tempObjMapped.first.first + 1][tempObjMapped.first.second] =
+              tempObjMapped.second;
+          if ((tempObjMapped.second == '[' or tempObjMapped.second == ']') and
+              not temp.contains({tempObjMapped.first.first - 1,
+                                 tempObjMapped.first.second})) {
+            map[tempObjMapped.first.first][tempObjMapped.first.second] = '.';
+          }
+        }
+        startPos.first++;
       }
-      map[startPos.first][startPos.second] = '.';
-      startPos.first++;
-      return;
+      return true;
     }
-    return;
+    if (map[objectToCheck.first][objectToCheck.second] == '[') {
+      objectsToMove.push_back(objectToCheck);
+      std::pair<int, int> secondObjectToCheck = {objectToCheck.first,
+                                                 objectToCheck.second + 1};
+      objectsToMove.push_back(secondObjectToCheck);
+      if (map[secondObjectToCheck.first - 1][secondObjectToCheck.second] ==
+          '.') {
+        objectsToMove.push_back(
+            {secondObjectToCheck.first - 1, secondObjectToCheck.second});
+      }
+      objectToCheck.first++;
+      secondObjectToCheck.first++;
+      return move(direction, objectToCheck, objectsToMove) and
+             move(direction, secondObjectToCheck, objectsToMove);
+    }
+    if (map[objectToCheck.first][objectToCheck.second] == ']') {
+      objectsToMove.push_back(objectToCheck);
+      std::pair<int, int> secondObjectToCheck = {objectToCheck.first,
+                                                 objectToCheck.second - 1};
+      objectsToMove.push_back(secondObjectToCheck);
+      if (map[secondObjectToCheck.first - 1][secondObjectToCheck.second] ==
+          '.') {
+        objectsToMove.push_back(
+            {secondObjectToCheck.first - 1, secondObjectToCheck.second});
+      }
+      objectToCheck.first++;
+      secondObjectToCheck.first++;
+      return move(direction, objectToCheck, objectsToMove) and
+             move(direction, secondObjectToCheck, objectsToMove);
+    }
+    if (map[objectToCheck.first][objectToCheck.second] == '.') {
+      return true;
+    }
+    return false;
   case left:
-    if (map[startPos.first][startPos.second - objectsToMove] == 'O') {
-      move(left, objectsToMove + 1);
-      return;
-    }
-    if (map[startPos.first][startPos.second - objectsToMove] == '.') {
-      for (int i = 0; i < objectsToMove; i++) {
-        map[startPos.first][startPos.second - objectsToMove + i] =
-            map[startPos.first][startPos.second - objectsToMove + (i + 1)];
+    if (objectToCheck == startPos) {
+      objectsToMove.push_back(objectToCheck);
+      objectToCheck.second--;
+      if (move(direction, objectToCheck, objectsToMove)) {
+        for (int i = objectsToMove.size() - 1; i >= 0; i--) {
+          auto object = objectsToMove[i];
+          map[object.first][object.second - 1] =
+              map[object.first][object.second];
+        }
+        startPos.second--;
       }
-      map[startPos.first][startPos.second] = '.';
-      startPos.second--;
-      return;
+      return true;
     }
-    return;
+    if (map[objectToCheck.first][objectToCheck.second] == '[' or
+        map[objectToCheck.first][objectToCheck.second] == ']') {
+
+      objectsToMove.push_back(objectToCheck);
+      objectToCheck.second--;
+      return move(direction, objectToCheck, objectsToMove);
+    }
+    if (map[objectToCheck.first][objectToCheck.second] == '.') {
+      return true;
+    }
+    return false;
   case right:
-    if (map[startPos.first][startPos.second + objectsToMove] == 'O') {
-      move(right, objectsToMove + 1);
-      return;
-    }
-    if (map[startPos.first][startPos.second + objectsToMove] == '.') {
-      for (int i = 0; i < objectsToMove; i++) {
-        map[startPos.first][startPos.second + objectsToMove - i] =
-            map[startPos.first][startPos.second + objectsToMove - (i + 1)];
+    if (objectToCheck == startPos) {
+      objectsToMove.push_back(objectToCheck);
+      objectToCheck.second++;
+      if (move(direction, objectToCheck, objectsToMove)) {
+        for (int i = objectsToMove.size() - 1; i >= 0; i--) {
+          auto object = objectsToMove[i];
+          map[object.first][object.second + 1] =
+              map[object.first][object.second];
+        }
+        startPos.second++;
       }
-      map[startPos.first][startPos.second] = '.';
-      startPos.second++;
-      return;
+      return true;
     }
-    return;
+    if (map[objectToCheck.first][objectToCheck.second] == '[' or
+        map[objectToCheck.first][objectToCheck.second] == ']') {
+
+      objectsToMove.push_back(objectToCheck);
+      objectToCheck.second++;
+      return move(direction, objectToCheck, objectsToMove);
+    }
+    if (map[objectToCheck.first][objectToCheck.second] == '.') {
+      return true;
+    }
+    return false;
   default:
-    return;
+    return false;
   }
 }
 
@@ -110,5 +228,14 @@ unsigned long long WarehouseWoes::calculateResultPart1() {
     }
   }
   return result;
-  ;
+}
+
+unsigned long long WarehouseWoes::calculateResultPart2() {
+  unsigned long long result{0};
+  for (int line = 0; line < map.size(); line++) {
+    for (int row = 0; row < map[line].size(); row++) {
+      result += map[line][row] == '[' ? 100 * line + row : 0;
+    }
+  }
+  return result;
 }
